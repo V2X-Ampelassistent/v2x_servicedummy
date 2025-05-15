76f5ec1faf5c5ff46d46e9283d2b4c8b9c0932c7
SchulzJonathan <notanemail@donotuse.home>
2025-05-15 11:11:44 +0200

    Added Service Dummy for Warning/Info Service.

--- v2x_servicedummy/servicedummynode.py
+++ v2x_servicedummy/servicedummynode.py
@@ -1,4 +1,4 @@
-from v2x_services.srv import TrafficLightInfo
+from v2x_services.srv import TrafficLightInfo, WarningInfoDisplay
 
 import rclpy
 from rclpy.node import Node
@@ -8,11 +8,18 @@ class ServiceDummyNode(Node):
     def __init__(self):
         super().__init__('ServiceDummyNode')
         self.srv = self.create_service(TrafficLightInfo, 'TrafficLightInfo', self.TrafficLightInfoCallback)
+        self.srv2 = self.create_service(WarningInfoDisplay, 'WarningInfoDisplay', self.WarningInfoDisplayCallback)
 
     def TrafficLightInfoCallback(self, request, response):
         # To trigger callback, run '''ros2 service call /TrafficLightInfo v2x_services/srv/TrafficLightInfo'''
         self.get_logger().info('received Request')
 
+        return response
+    
+    def WarningInfoDisplayCallback(self, request, response):
+        # To trigger callback, run '''ros2 service call /WarningInfoDisplay v2x_services/srv/WarningInfoDisplay "{type: 1, infomessage: 'This is a Message.'}"'''
+        self.get_logger().info(str(request))
+        response.ignored = True
         return response
 
 def main(args=None):
